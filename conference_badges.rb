332ddf47a65047c36c5f08af7890a633db99a0c9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-28 21:23:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,6 +21,8 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  attendees.each do |name|
+    puts batch_badge_creator(name)
+    puts assign_rooms(name)
+  end
 end
