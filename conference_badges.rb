07fe5bc9cc711b9a3f67aa087723e7b6cbeb5458
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-28 21:04:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  badge_array = []
+  array.each do |name|
+    badge_array << badge_maker(name)
+  end
+  return badge_array
+end
